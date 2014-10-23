690d6d4745b0d189e554f1aecaa49436e9733963
Charlie Heselton <cheselto@semprautilities.com>
2014-10-23 13:04:13 -0700

    Second try.

---
+++ malwr.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'colorize'
+require 'net/http'
+
+# save the file downloaded in the HTTP response
+def save_malware
+
+end
+
+# get a list of urls from RSS XML description
+
+# get a list of urls from RSS XML title
+
+# get a lit of urls from a simple text list
+
+# get a list of urls from urlquery.com
+
+### Main ###
