9a248d53c45641193d4a0a026d57081b2b33d185
dataking <dataking@gmail.com>
2017-03-21 12:57:44 -0700

    adding windows specific scripts

---
+++ categorizer.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET smallstr="\small"
+SET mediumstr="\medium"
+SET largestr="\large"
+SET xlargestr="\xlarge"
+SET smallfile="50001"
+SET mediumfile="1000001"
+SET largefile="6000001"
+SET root_dir="pulls"
+SET all_files=%root_dir%
+
+for %%f in '%all_files%' do (
+	
+)
---
+++ categorizer.ps1
@@ -0,0 +1,11 @@
+
+$smallstr="\small"
+$mediumstr="\medium"
+$largestr="\large"
+$xlargestr="\xlarge"
+$smallfile=50001
+$mediumfile=1000001
+$largefile=6000001
+$root_dir="pulls"
+$all_files=$root_dir
+
